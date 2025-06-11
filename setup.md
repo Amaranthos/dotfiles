50111026004d30ff5301d94cda5452485a9aafa3
Joshua Hodkinson <jhodkinson@atlassian.com>
2025-06-11 15:55:10 +1000

    Updates

--- setup.md
+++ setup.md
@@ -10,7 +10,10 @@ brew bundle install
 
 ## Configure bash
 
-https://itnext.io/upgrading-bash-on-macos-7138bd1066ba
+<https://itnext.io/upgrading-bash-on-macos-7138bd1066ba>
+<https://www.unindented.org/blog/change-shell-to-latest-bash-on-macos/>
 
-- white list bash@5 `sudo nano /etc/shells/`
-- set default shell `chsh -s <path>`
+<!-- -   white list bash@5 `sudo nano /etc/shells/` -->
+
+-   sudo sh -c 'echo /opt/homebrew/bin/bash >> /etc/shells'
+-   set default shell `chsh -s /opt/homebrew/bin/bash`
--- setup.sh
+++ setup.sh
@@ -1,15 +1,23 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 # xcode-select —-install
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# Pull dotfiles
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+# brew analytics off
+# brew tap bashdot/tap
+# brew install bashdot
+# git pull https://github.com/Amaranthos/dotfiles.git
+# env USER_EMAIL="" bashdot install default
 # brew bundle install
+
+osascript -e 'tell application "System Preferences" to quit'
+
+sudo -v
 
 /usr/sbin/softwareupdate --install-rosetta --agree-to-license
 
 # macOS defaults
 ## Finder
-
 chflags nohidden ~/Library
 defaults write com.apple.finder AppleShowAllFiles true
 defaults write com.apple.finder AppleShowAllExtensions true
@@ -18,9 +26,9 @@ defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.finder QuitMenuItem -bool true
+defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/"
 
 ## Dock
-
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-time-modifier -float 0.2
 defaults write com.apple.dock autohide-delay -float 0
@@ -29,11 +37,48 @@ defaults write com.apple.dock mineffect -string "scale"
 defaults write com.apple.dock minimize-to-application -bool true
 defaults write com.apple.dock expose-animation-duration -float 0.05
 
-## Desktop Services
+## Stage manager
+defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
 
+## Desktop Services
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
+## Siri
+defaults write com.apple.assistant.support "Assistant Enabled" -bool false
+defaults write com.apple.Siri StatusMenuVisible -bool false
+defaults write com.apple.Siri UserHasDeclinedEnable -bool false
+
+## Spotlight
+defaults write com.apple.spotlight orderedItems -array \
+	'{"enabled" = 1;"name" = "APPLICATIONS";}' \
+	'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+	'{"enabled" = 1;"name" = "DIRECTORIES";}' \
+	'{"enabled" = 1;"name" = "PDF";}' \
+	'{"enabled" = 1;"name" = "FONTS";}' \
+	'{"enabled" = 0;"name" = "DOCUMENTS";}' \
+	'{"enabled" = 0;"name" = "MESSAGES";}' \
+	'{"enabled" = 0;"name" = "CONTACT";}' \
+	'{"enabled" = 0;"name" = "EVENT_TODO";}' \
+	'{"enabled" = 0;"name" = "IMAGES";}' \
+	'{"enabled" = 0;"name" = "BOOKMARKS";}' \
+	'{"enabled" = 0;"name" = "MUSIC";}' \
+	'{"enabled" = 0;"name" = "MOVIES";}' \
+	'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+	'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+	'{"enabled" = 0;"name" = "SOURCE";}' \
+	'{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
+	'{"enabled" = 0;"name" = "MENU_OTHER";}' \
+	'{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
+	'{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
+	'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
+	'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+# Load new settings before rebuilding the index
+killall mds > /dev/null 2>&1
+# Make sure indexing is enabled for the main volume
+sudo mdutil -i on / > /dev/null
+# Rebuild the index from scratch
+sudo mdutil -E / > /dev/null
 
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 defaults write -g NSNavPanelExpandedStateForSaveMode2 -bool true
@@ -47,7 +92,6 @@ defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
 defaults write -g NSToolbarTitleViewRolloverDelay -float 0
 
 ## Keyboard
-
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g KeyRepeat -int 1
@@ -57,6 +101,10 @@ defaults write -g AppleShowScrollBars -string "always"
 
 defaults write -g com.apple.swipescrolldirection -bool false
 defaults write -g com.apple.mouse.scaling -1
+defaults write -g com.apple.keyboard.fnState -bool true
+
+# Default apps
+open -a "Thorium" --args --make-default-browser
 
 killall Finder
 killall Dock
